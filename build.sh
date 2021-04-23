81cf8c5bd51ab5c8c6bb15e058d41fe88ff06bde
Eduard Gómez <eduard.gomez.escandell@gmail.com>
2021-04-23 13:37:02 +0200

    Fixed some build issues

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 PROJECT_DIR=$PWD
 
-if [[$1 -z]]
+if $1 -z
 then
 	BUILD_TYPE=Release
 else
@@ -12,7 +12,7 @@ BUILD_DIR=$PROJECT_DIR/build
 SOURCE_DIR=$PROJECT_DIR/source
 EXECUTABLE=ConsoleChess
 
-echo "Building $EXECUTABLE"
+echo "Building $EXECUTABLE in $BUILD_TYPE mode"
 
 mkdir $BUILD_DIR
 cd $BUILD_DIR
@@ -23,3 +23,6 @@ cmake $SOURCE_DIR -DCMAKE_BUILD_TYPE=$BUILD_TYPE
 cmake --build .
 
 cd $PROJECT_DIR
+
+echo
+echo "Your executable is at $BUILD_DIR/$BUILD_TYPE"
